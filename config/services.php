fa585917c9f20035eae0478c4da74e6b2ab46b8d
Danniel Libor <dlibor.dev@gmail.com>
2025-09-30 18:24:59 +0800

    fix: update Google Cloud credentials path in environment and services configuration

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'google_cloud' => [
         'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
         'bucket'     => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
-        'key_file'   => storage_path('app/credentials/snap-csg-229610-f2bb5c5c6b21.json'),
+        'key_file'   => env('GOOGLE_CLOUD_KEY_PATH'), // storage_path('app/credentials/snap-csg-229610-f2bb5c5c6b21.json'),
     ],
 
     'oracle' => [
