f0156275a11c7ebd808c9f78bff4830d6aec19e3
Danniel Libor <dlibor.dev@gmail.com>
2025-10-20 22:26:27 +0800

    fix: update iframe source to use config URL

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,4 +1,4 @@
 <x-layouts.app>
-    <iframe src="http://localhost:8000/app/inquiries/create" class="w-full h-[600px] rounded-xl border-none"
+    <iframe src="{{ config('app.url') }}/app/inquiries/create" class="w-full h-[600px] rounded-xl border-none"
         frameborder="0" allowFullScreen="true"></iframe>
 </x-layouts.app>
