9f678d4b4f6ac266158252bd6048d50ed290312b
Danniel Libor <dlibor.dev@gmail.com>
2025-12-10 21:28:38 +0800

    refactor(ListReports.php, ReportForm.php): clean up file upload actions and reconfigure report form

--- app/Filament/Resources/Reports/Schemas/ReportForm.php
+++ app/Filament/Resources/Reports/Schemas/ReportForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Reports\Schemas;
 
+use Filament\Forms\Components\FileUpload;
 use Filament\Schemas\Schema;
 
 class ReportForm
@@ -10,7 +11,16 @@ class ReportForm
     {
         return $schema
             ->components([
-                //
+                FileUpload::make('files')
+                    ->label('Select Report CSV Files')
+                    ->multiple()
+                    ->maxFiles(50)
+                    ->storeFiles(false)
+                    ->disk(config('filesystems.default'))
+                    ->acceptedFileTypes(['text/csv', '.csv'])
+                    ->panelLayout('grid')
+                    ->visibility('private')
+                    ->required(),
             ]);
     }
 }
